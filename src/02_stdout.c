4b5e9ec5b49fe68ee26e795d947fdcdd20dfe915
Niyamaka <niyamaka@yuneta.io>
2020-03-21 13:13:26 +0100

    improve stdout_write() and stdout_fwrite()

--- src/02_stdout.c
+++ src/02_stdout.c
@@ -46,65 +46,48 @@ PRIVATE const char *priority_names[]={
  ***************************************************************************/
 PUBLIC int stdout_write(void* v, int priority, const char* bf, size_t len)
 {
-    static char buffer[1*1024*1024];
     if(!bf) {
-        print_error(
-            PEF_CONTINUE,
-            "ERROR YUNETA",
-            "stdout_write(): bf NULL"
-        );
+        // silence
         return -1;
     }
     if(len<=0) {
-        print_error(
-            PEF_CONTINUE,
-            "ERROR YUNETA",
-            "stdout_write(): len <= 0"
-        );
+        // silence
         return -1;
     }
     if(priority < 0 || priority > LOG_MONITOR) {
         priority = LOG_DEBUG;
     }
 
-//     if(priority == LOG_DEBUG) {
-//         snprintf(
-//             buffer,
-//             sizeof(buffer),
-//             "%.*s\n",
-//             (int)len,
-//             bf
-//         );
-//     } else {
-        snprintf(
-            buffer,
-            sizeof(buffer),
-            "%s: %.*s\n",
-            priority_names[priority],
-            (int)len,
-            bf
-        );
-//     }
-    fwrite(buffer, strlen(buffer), 1, stdout);
+    fwrite(priority_names[priority], strlen(priority_names[priority]), 1, stdout);
+    fwrite(": ", strlen(": "), 1, stdout);
+    fwrite(bf, len, 1, stdout);
+    fwrite("\n", strlen("\n"), 1, stdout);
+
     return 0;
 }
 
 /***************************************************************************
  *
  ***************************************************************************/
-PUBLIC int stdout_fwrite(void *v, int priority, const char *format, ...)
+PUBLIC int stdout_fwrite(void *v, int priority, const char *fmt, ...)
 {
     va_list ap;
-    static char temp[8*1024];
+    int length;
+    char *buf;
 
-    va_start(ap, format);
-    vsnprintf(
-        temp,
-        sizeof(temp),
-        format,
-        ap
-    );
+    va_start(ap, fmt);
+
+    length = vsnprintf(NULL, 0, fmt, ap);
+    if(length>0) {
+        buf = malloc(length + 1);
+        if(buf) {
+            vsnprintf(buf, length + 1, fmt, ap);
+            stdout_write(v, priority, buf, strlen(buf));
+            free(buf);
+        }
+    }
+
     va_end(ap);
 
-    return stdout_write(v, priority, temp, strlen(temp));
+    return 0;
 }
