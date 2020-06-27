6bcd705126012b4e76b0b5d5a6541e7ac4995baf
Niyamaka <niyamaka@yuneta.io>
2020-06-27 20:28:37 +0200

    v4.2.0 with tls

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.1.4"
+#define __ghelpers_version__  "4.2.0"
 
 
 #ifdef __cplusplus
