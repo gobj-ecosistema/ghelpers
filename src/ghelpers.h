af361d0f7dc5febf93d9d9780c62d4b3d1e40339
Niyamaka <niyamaka@yuneta.io>
2020-07-04 12:45:35 +0200

    v4.2.1 fix websocket and httparser

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.2.0"
+#define __ghelpers_version__  "4.2.1"
 
 
 #ifdef __cplusplus
