130d81b87e1e37365537e15c8c315b2f9063776c
Niyamaka <niyamaka@yuneta.io>
2020-12-09 16:13:17 +0100

    yuneta version 4.2.28, all node's api has changed

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.2.27"
+#define __ghelpers_version__  "4.2.28"
 
 
 #ifdef __cplusplus
