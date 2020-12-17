1a0edd21bbb70d818f964144335caeb502858325
Niyamaka <niyamaka@yuneta.io>
2020-12-17 18:27:06 +0100

    meteré las authz como comando

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.3.3"
+#define __ghelpers_version__  "4.3.4"
 
 
 #ifdef __cplusplus
