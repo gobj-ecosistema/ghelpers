57015a581f10f6f8b5e41f57819a22ce9cf5f5f9
Niyamaka <niyamaka@yuneta.io>
2020-12-21 09:55:55 +0100

    v4.4.0 include new authz api

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.3.4"
+#define __ghelpers_version__  "4.4.0"
 
 
 #ifdef __cplusplus
