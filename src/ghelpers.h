a5c1740554278bfe0f7221adf3a5c2309eacfc9a
Niyamaka <niyamaka@yuneta.io>
2021-01-03 16:57:01 +0100

    v4.5.0 Refactoring realms authz

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.4.1"
+#define __ghelpers_version__  "4.5.0"
 
 
 #ifdef __cplusplus
