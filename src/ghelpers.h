8f987e4abf2320a7c19655438bf764b98b0eab43
Niyamaka <niyamaka@yuneta.io>
2021-01-14 10:15:14 +0100

    v4.6.7 "Working without authentication" cannot return -1 because deny access.

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.6.6"
+#define __ghelpers_version__  "4.6.7"
 
 
 #ifdef __cplusplus
