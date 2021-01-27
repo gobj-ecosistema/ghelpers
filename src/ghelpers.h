2bbd23d8956c2d68035a7ada01516f2e1178aab5
Niyamaka <niyamaka@yuneta.io>
2021-01-27 09:01:11 +0100

    v4.7.7 API gobj_link_nodes/gobj_unlink_nodes changed, must include names of topics

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.7.6"
+#define __ghelpers_version__  "4.7.7"
 
 
 #ifdef __cplusplus
