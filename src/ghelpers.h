1e7438654893a6e77355094bf2c3496dafc5e2bf
Niyamaka <niyamaka@yuneta.io>
2021-02-16 13:51:53 +0100

    v4.9.6

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.9.5"
+#define __ghelpers_version__  "4.9.6"
 
 
 #ifdef __cplusplus
