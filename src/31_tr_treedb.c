93dfff918cbd74d59771b5ce2096d6c6243a389b
Niyamaka <niyamaka@yuneta.io>
2021-02-26 20:11:14 +0100

    add enum to list of enum

--- src/31_tr_treedb.c
+++ src/31_tr_treedb.c
@@ -456,13 +456,13 @@ PUBLIC json_t *_treedb_create_topic_cols_desc(void)
     );
     json_array_append_new(
         topic_cols_desc,
-        json_pack("{s:s, s:s, s:i, s:s, s:[s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s], s:[s,s,s]}",
+        json_pack("{s:s, s:s, s:i, s:s, s:[s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s], s:[s,s,s]}",
             "id", "flag",
             "header", "Flag",
             "fillspace", 14,
             "type", "array",
             "enum",
-                "persistent","required","fkey",
+                "persistent","required","fkey","enum",
                 "hook","uuid","notnull","wild","rowid","inherit",
                 "readable","writable","stats","rstats","pstats",
                 "password","email","url","time", "color",
