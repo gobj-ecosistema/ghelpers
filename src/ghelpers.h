f4b9a0e2dbad8f89e179a9ad884701ddca441017
Niyamaka <niyamaka@yuneta.io>
2021-03-26 09:09:26 +0100

    v4.10.16 fix openssl

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.10.15"
+#define __ghelpers_version__  "4.10.16"
 
 
 #ifdef __cplusplus
