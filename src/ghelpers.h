1907c9634498c48a5c9afe8a5cf9f218fa9ad215
Niyamaka <niyamaka@yuneta.io>
2021-04-10 17:03:49 +0200

    v4.11.1 update libuv

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.11.0"
+#define __ghelpers_version__  "4.11.1"
 
 
 #ifdef __cplusplus
