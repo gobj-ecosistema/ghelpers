a503b5a4bc968110aae4e303d1c18a76794263dc
Niyamaka <niyamaka@yuneta.io>
2021-06-16 19:24:39 +0200

    v4.14.0 A new feature: node owner, the owner of a (prod/staging/test/dev) node governed by a yuneta agent, WARNING gobj_yuno_factory() api has changed

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -70,7 +70,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.13.3"
+#define __ghelpers_version__  "4.14.0"
 
 
 #ifdef __cplusplus
