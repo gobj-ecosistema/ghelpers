f98ac68e652c65da7b310607274e352d1add24bf
Niyamaka <niyamaka@yuneta.io>
2021-07-26 10:50:49 +0200

    fix node_owner

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -67,7 +67,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.15.2"
+#define __ghelpers_version__  "4.15.3"
 
 
 #ifdef __cplusplus
