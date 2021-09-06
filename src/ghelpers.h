bef8adf2c2e9d34c8d2bc043949ed84f77925678
Niyamaka <niyamaka@yuneta.io>
2021-09-06 13:57:56 +0200

    v4.16.0 task_authentication does the logout

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -67,7 +67,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.15.9"
+#define __ghelpers_version__  "4.16.0"
 
 
 #ifdef __cplusplus
