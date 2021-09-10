9f8d37b7f1769ec03f2267d6ad83aea2b5930bea
Niyamaka <niyamaka@yuneta.io>
2021-09-10 10:51:56 +0200

    v4.17.2 Postgres gclass is now an Object with __queries_in_queue__

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -67,7 +67,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.17.1"
+#define __ghelpers_version__  "4.17.2"
 
 
 #ifdef __cplusplus
