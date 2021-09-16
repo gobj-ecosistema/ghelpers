616b0b85e32815bcd603db81704ad34f9f7fdb41
Niyamaka <niyamaka@yuneta.io>
2021-09-16 19:32:46 +0200

    v4.18.2 logcenter informa del node uuid

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -67,7 +67,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.18.1"
+#define __ghelpers_version__  "4.18.2"
 
 
 #ifdef __cplusplus
