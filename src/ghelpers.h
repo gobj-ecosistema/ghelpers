f4646e4a9d5714612a8c004e64582df9ed2e357b
Niyamaka <niyamaka@yuneta.io>
2021-09-21 11:16:07 +0200

    fix bug cli

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -67,7 +67,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.19.0"
+#define __ghelpers_version__  "4.19.1"
 
 
 #ifdef __cplusplus
