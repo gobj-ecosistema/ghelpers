cb21634e82b000620d5f2dcd781df41155f8d029
Niyamaka <niyamaka@yuneta.io>
2021-11-03 11:18:42 +0100

    v4.22.1 FIX register/find public services in agent

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -67,7 +67,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "4.22.0"
+#define __ghelpers_version__  "4.22.1"
 
 
 #ifdef __cplusplus
