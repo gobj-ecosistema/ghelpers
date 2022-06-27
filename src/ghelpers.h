eb485083305a62b6c5ee2e5148bfa03ecb160cdb
ginsmar <ginsmar@gmail.com>
2022-06-27 14:18:06 +0200

    v5.8.9 fix yuneta_agent22

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -67,7 +67,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "5.8.8"  /* XX__yuneta_version__XX */
+#define __ghelpers_version__  "5.8.9"  /* XX__yuneta_version__XX */
 
 
 #ifdef __cplusplus
