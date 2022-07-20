352385b1b58c85a231ad28b846d58855e600a9f3
ginsmar <ginsmar@gmail.com>
2022-07-20 06:45:36 +0200

    compiler options external libs

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -67,7 +67,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "5.9.1"  /* XX__yuneta_version__XX */
+#define __ghelpers_version__  "5.9.2"  /* XX__yuneta_version__XX */
 
 
 #ifdef __cplusplus
