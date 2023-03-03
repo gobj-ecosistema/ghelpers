2690f997da36b8e272a5b8999a82cd53f2522a7c
niyamaka <ginsmar@gmail.com>
2023-03-03 16:56:11 +0100

    yuneta version 6.3.5

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -66,7 +66,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "6.3.4"  /* XX__yuneta_version__XX */
+#define __ghelpers_version__  "6.3.5"  /* XX__yuneta_version__XX */
 
 
 #ifdef __cplusplus
