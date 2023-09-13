fa9c56e94572f3dfc7938aa67d3b2c76df64a6f7
niyamaka <ginsmar@gmail.com>
2023-09-13 17:54:05 +0200

    repon 6.4.0

--- src/ghelpers.h
+++ src/ghelpers.h
@@ -66,7 +66,7 @@ PUBLIC void end_ghelpers_library(void);
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __ghelpers_version__  "6.5.0"  /* XX__yuneta_version__XX */
+#define __ghelpers_version__  "6.4.0"  /* XX__yuneta_version__XX */
 
 
 #ifdef __cplusplus
