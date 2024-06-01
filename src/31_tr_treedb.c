3b260be2021fc9bf9daf87102973a8d1e4eda9c4
rmm <rosa.martinez@artgins.com>
2024-06-01 17:58:24 +0200

    rename name by topic

--- src/31_tr_treedb.c
+++ src/31_tr_treedb.c
@@ -853,13 +853,13 @@ PUBLIC json_t *treedb_open_db( // WARNING Return IS NOT YOURS!
      *      __graphs__
      *-------------------------------*/
     char *graphs_topic_name = "__graphs__";
-    int graphs_topic_version = 5;
+    int graphs_topic_version = 6;
     json_t *jn_graphs_topic_var = json_object();
     json_object_set_new(jn_graphs_topic_var, "topic_version", json_integer(graphs_topic_version));
 
     json_t *tag_graphs_schema = json_pack(
         "{s:{s:s, s:s, s:i, s:s, s:[s,s,s]},"       /* id */
-            "s:{s:s, s:s, s:i, s:s, s:[s,s,s]},"    /* name */
+            "s:{s:s, s:s, s:i, s:s, s:[s,s,s]},"    /* topic */
             "s:{s:s, s:s, s:i, s:s, s:[s,s,s]},"    /* active */
             "s:{s:s, s:s, s:i, s:s, s:[s,s,s,s]},"  /* time */
             "s:{s:s, s:s, s:i, s:s, s:[s,s]}}",     /* properties */
@@ -871,9 +871,9 @@ PUBLIC json_t *treedb_open_db( // WARNING Return IS NOT YOURS!
             "flag",
                 "persistent","required","rowid",
 
-        "name",
-            "id", "name",
-            "header", "Name",
+        "topic",
+            "id", "topic",
+            "header", "Topic",
             "fillspace", 28,
             "type", "string",
             "flag",
