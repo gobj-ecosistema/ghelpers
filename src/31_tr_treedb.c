88bda3146627e708c06a19b5fb20c3331f9c7759
rmm <rosa.martinez@artgins.com>
2024-06-03 09:55:38 +0200

    add treedb field to __graphs__

--- src/31_tr_treedb.c
+++ src/31_tr_treedb.c
@@ -854,16 +854,17 @@ PUBLIC json_t *treedb_open_db( // WARNING Return IS NOT YOURS!
      *      __graphs__
      *-------------------------------*/
     char *graphs_topic_name = "__graphs__";
-    int graphs_topic_version = 9;
+    int graphs_topic_version = 10;
     json_t *jn_graphs_topic_var = json_object();
     json_object_set_new(jn_graphs_topic_var, "topic_version", json_integer(graphs_topic_version));
 
     json_t *tag_graphs_schema = json_pack(
-        "{s:{s:s, s:s, s:i, s:s, s:[s,s,s]},"       /* id */
-            "s:{s:s, s:s, s:i, s:s, s:[s,s,s]},"    /* topic */
-            "s:{s:s, s:s, s:i, s:s, s:[s,s]},"      /* active */
-            "s:{s:s, s:s, s:i, s:s, s:[s,s,s,s,s]},"  /* time */
-            "s:{s:s, s:s, s:i, s:s, s:[s,s]}}",     /* properties */
+        "{s:{s:s, s:s, s:i, s:s, s:[s,s,s]},"           /* id */
+            "s:{s:s, s:s, s:i, s:s, s:[s,s,s]},"        /* treedb */
+            "s:{s:s, s:s, s:i, s:s, s:[s,s,s]},"        /* topic */
+            "s:{s:s, s:s, s:i, s:s, s:[s,s]},"          /* active */
+            "s:{s:s, s:s, s:i, s:s, s:[s,s,s,s,s]},"    /* time */
+            "s:{s:s, s:s, s:i, s:s, s:[s,s]}}",         /* properties */
         "id",
             "id", "id",
             "header", "RowId",
@@ -872,6 +873,13 @@ PUBLIC json_t *treedb_open_db( // WARNING Return IS NOT YOURS!
             "flag",
                 "persistent","required","rowid",
 
+        "treedb",
+            "id", "treedb",
+            "header", "Treedb",
+            "fillspace", 28,
+            "type", "string",
+            "flag",
+                "persistent", "required", "writable",
         "topic",
             "id", "topic",
             "header", "Topic",
