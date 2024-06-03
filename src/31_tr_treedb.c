84ca1698ec30930a7b3d5da9cb28487a91c6a773
rmm <rosa.martinez@artgins.com>
2024-06-03 09:06:41 +0200

    add 'now' flag

--- src/31_tr_treedb.c
+++ src/31_tr_treedb.c
@@ -467,7 +467,7 @@ PUBLIC json_t *_treedb_create_topic_cols_desc(void)
     );
     json_array_append_new(
         topic_cols_desc,
-        json_pack("{s:s, s:s, s:i, s:s, s:[s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s], s:[s,s,s]}",
+        json_pack("{s:s, s:s, s:i, s:s, s:[s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s], s:[s,s,s]}",
             "id", "flag",
             "header", "Flag",
             "fillspace", 14,
@@ -493,6 +493,7 @@ PUBLIC json_t *_treedb_create_topic_cols_desc(void)
                 "email",
                 "url",
                 "time",
+                "now",
                 "color",
                 "image",
                 "tel",
